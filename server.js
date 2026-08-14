3aa7c85848b2dd67fb6d4d16e8980eef44b8e680
varsha <varshavijayaragavan28@gmail.com>
2026-08-14 09:56:40 +0530

    Changes

--- server.js
+++ server.js
@@ -11,11 +11,14 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "lifelink_db",
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 // ============================================================
